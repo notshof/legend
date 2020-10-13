0538e6a8c6d1911900d19f84584eeada20f9032c
notshof <53849196+notshof@users.noreply.github.com>
2020-10-13 17:11:48 +1100

    Update approve.sh

--- approve.sh
+++ approve.sh
@@ -54,15 +54,6 @@ if [ -n "${APPROVALS_LIST}" ] && echo "${APPROVALS_LIST}" | jq '.value[]' > /dev
     APPROVAL_ID=$(echo "${APPROVALS_LIST}" | jq '.value[] | select(.release.id=='"${RELEASE_ID}"') | .id')
     if [ -n "${APPROVAL_ID}" ]; then
         echo "Approval ID found: ${APPROVAL_ID}"
-        if curl --header "Content-Type: application/json" --data '{"status": "approved","comments": "ServiceNow request has been approved."}' -u ":${AZURE_PAT}" --request PATCH "https://vsrm.dev.azure.com/${AZURE_ORG}/${AZURE_PROJECT}/_apis/release/approvals/${APPROVAL_ID}?api-version=6.0" | grep "approvedBy" > /dev/null 2>&1; then
-            echo "Successfully approved release to production."
-        else
-            echo "Failed to approve release to production."
-            exit 1
-        fi
-    else
-        echo "Approval ID not found"
-        exit 1
     fi
 else
     echo -e "\nFailed to fetch approvals list or approvals list is empty.\n\nError: \n${APPROVALS_LIST}"
